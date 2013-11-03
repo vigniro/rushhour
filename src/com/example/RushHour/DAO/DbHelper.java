8757f88f2def9c60daeb328c66d9683e783177fe
Víðir Orri Reynisson <vidirr@gmail.com>
2013-11-03 17:11:35 +0000

    Added getters and setters for current_level table.

--- src/com/example/RushHour/DAO/DbHelper.java
+++ src/com/example/RushHour/DAO/DbHelper.java
@@ -15,12 +15,11 @@ public class DbHelper extends SQLiteOpenHelper {
     public static final String TABLE_LEVELS_FINISHED = "levels_finished";
     public static final String COLUMN_ID = "_id";
 
-    public static final String TABLE_CURRENT_POSITION = "current_position";
-    public static final String COLUMN_CURRENT_LEVEL = "_level_id";
-    public static final String COLUMN_POSITON = "_position";
+    public static final String TABLE_CURRENT_LEVEL = "current_level";
 
 
-    public static final String[] TableLevelsFinishedCols = {"_id"};
+    public static final String[] TableLevelsFinishedCols = {COLUMN_ID};
+    public static final String[] TableCurrentLevelCols = {COLUMN_ID};
 
     private static final String DATABASE_NAME = "RushHour.db";
     private static final int DATABASE_VERSION = 1;
@@ -28,11 +27,9 @@ public class DbHelper extends SQLiteOpenHelper {
     // Database creation sql statement
     private static final String DATABASE_CREATE = "create table "
             //Table that simply stores the ID's of levels that the player has finished.
-            + TABLE_LEVELS_FINISHED + "(" + COLUMN_ID
-            + " integer unique);"
-            //Table to store the current layout of the playing board, contains level ID and a string representation of the board.
-            + "create table " + TABLE_CURRENT_POSITION + "(" + COLUMN_CURRENT_LEVEL + " integer unique, "
-            + COLUMN_POSITON + "text not null);";
+            + TABLE_LEVELS_FINISHED + "(" + COLUMN_ID + " integer unique);"
+            //Table to store the current level we're on. Should only hold 1 record at all times.
+            + "create table " + TABLE_CURRENT_LEVEL + "(" + COLUMN_ID + " integer unique);";
 
     public DbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -41,6 +38,9 @@ public class DbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase database) {
         database.execSQL(DATABASE_CREATE);
+        //Initialize the current_level table with the ID of the first level - since if we're creating the DB that means that the player is on level 0.
+        //Levels are 0-based indexed.
+        database.execSQL("INSERT INTO " + TABLE_CURRENT_LEVEL + " VALUES(1, 0)");
     }
 
     @Override
@@ -49,13 +49,13 @@ public class DbHelper extends SQLiteOpenHelper {
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_LEVELS_FINISHED);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CURRENT_POSITION);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CURRENT_LEVEL);
         onCreate(db);
     }
 
     public void reinitDatabase(SQLiteDatabase db) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_LEVELS_FINISHED);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CURRENT_POSITION);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CURRENT_LEVEL);
         onCreate(db);
     }
 
